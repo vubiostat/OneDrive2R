063c46dbf86fa9983bd016111097946373662f91
James Grindstaff <james.grindstaff@vumc.org>
2024-10-03 13:55:12 -0500

    Fixed loading items at top level. Also fixed rds and rdata loading.

--- example.R
+++ example.R
@@ -1,5 +1,6 @@
 library(Microsoft365R)
- 
+library(readxl)
+
 od <- get_business_onedrive(auth_type="device_code")
  
 load_shared_dataframe <- function(od, path)
@@ -8,31 +9,47 @@ load_shared_dataframe <- function(od, path)
    shared_files <- od$do_operation("sharedWithMe", options=list(allowexternal="true"))
  
    objs <- lapply(shared_files$value, function(obj) obj$remoteItem)
-  
-   matching_folder <- objs[[which(unlist(lapply(objs, function(obj) obj$name %in% items[1])))]]
-  
    directory_depths <- items[-c(1, length(items))]
-  
+
+   matching_folder <- objs[[which(unlist(lapply(objs, function(obj) obj$name %in% items[1])))]]
+   
    result <- ms_drive_item$new(od$token, od$tenant, matching_folder)
- 
+   
    for (d in directory_depths) {
      result <- result$get_item(d)
    }
   
    type = "df"
   
-   if("rds" %in% items[length(items)]) {
+   if(grepl("rds", items[length(items)])) {
      type = "rds"
    }
- 
+   
+   if(grepl("RData", items[length(items)])) {
+     type = "rdata"
+   }
+   
    if(type == "df") {
-     result <- result$get_item(items[length(items)])$load_dataframe()
+     if(length(directory_depths) > 0) {
+       result <- result$get_item(items[length(items)])$load_dataframe()
+     } else {
+       result <- result$load_dataframe()
+     }
    } else if(type == "rds") {
-     result <- result$get_item(items[length(items)])$load_rds()
+     if(length(directory_depths) > 0) {
+       result <- result$get_item(items[length(items)])$load_rds()
+     } else {
+       result <- result$load_rds()
+     }
    } else if(type == "rdata") {
-     result <- result$get_item(items[length(items)])$load_rdata()
+     if(length(directory_depths) > 0) {
+       result <- result$get_item(items[length(items)])$load_rdata()
+     } else {
+       result <- result$load_rdata()
+     }
    }
 }
  
 x <- load_shared_dataframe(od, 'parentfolder/subfolder/item.csv')
+
 x
