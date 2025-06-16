bb790308a8e97e51656a0b8307b7341d9626e59a
Shawn Garbett <shawn@garbett.org>
2025-06-16 11:12:56 -0500

    One more refactor. Not much left to squeeze out.

--- example.R
+++ example.R
@@ -1,12 +1,13 @@
 library(Microsoft365R)
 library(readxl)
+library(foreign)
 
 # Example authentication
-# auth <- get_business_onedrive(auth_type="device_code")
+# drive <- get_business_onedrive(drive_type="device_code")
 
-shared <- function(auth)
+shared <- function(drive)
 {
-  shared_objs <- auth$do_operation("sharedWithMe",
+  shared_objs <- drive$do_operation("sharedWithMe",
                                     options=list(allowexternal="true"))
   objs <- lapply(shared_objs$value, function(obj) obj$remoteItem)
   names(objs)  <- sapply(objs, function(o) o$name)
@@ -14,46 +15,50 @@ shared <- function(auth)
   objs
 }
 
-one_drive_2_R <- function(auth, path, use.readr=FALSE, FUN=NULL, ...)
+one_drive_2_R <- function(drive, path, FUN=NULL, ...)
 {
-  filename     <- basename(path)
-  items        <- strsplit(dirname(path), "/")[[1]]
-  path_len     <- length(items)
-  # case: "path" is top-level filename
-  if(path_len == 1 && items == '.')
+  filename <- basename(path)
+  items    <- strsplit(dirname(path), "/")[[1]]
+  objs     <- shared(drive)
+  top      <- objs[[items[1]]]
+
+  if(is.null(top)) stop(paste("Requested top level of path must be shared name. Check: `names(shared(drive))`"))
+  # There _should_ be a better way than calling new, but alas
+  item     <- ms_drive_item$new(drive$token, drive$tenant, top)
+  if(length(items) > 1)
   {
-    items <- fp <- filename
-  } else
-  {
-    fp <- do.call(file.path, as.list(c(items[-1], filename)))
-    items <- items[1]
+      fp     <- do.call(file.path, as.list(c(items[-1], filename)))
+      item   <- item$get_item(fp)
   }
-  objs         <- shared(auth)
-  top <- objs[[items]]
-
-  if(is.null(top))
-    stop(paste("Requested top level of path must be shared name. Check: `names(shared(auth))`"))
-
-  # there must be a better way to set the root directory to a shared folder
-  item <- ms_drive_item$new(auth$token, auth$tenant, top)
-  if(path_len > 1) item <- item$get_item(fp)
 
   type <- tolower(tools::file_ext(filename))
+  if(is.null(FUN))
+  {
+    FUN <- switch(type,
+      arff  = foreign::read.arff,
+      csv   = utils::read.csv,
+      dbf   = foreign::read.dbf,
+      dta   = foreign::read.dta,
+      m     = foreign::read.octave,
+      mtp   = foreign::read.mtp,
+      rds   = readRDS,
+      rdata = load,
+      rec   = foreign::read.epiinfo,
+      spss  = foreign::read.spss,
+      syd   = ,
+      sys   = foreign::read.systat,
+      table = stats::read.ftable,
+      xls   = ,
+      xlsx  = readxl::read_excel,
+      xpt   = foreign::read.xport,
+      NULL
+    )
+    if(is.null(FUN)) stop(paste0("Unhandled File Extension '", type, "'"))
+  }
+
   infile <- tempfile(fileext = paste0('.', type))
   on.exit(unlink(infile))  # This is the auto delete
   item$download(dest = infile)
 
-  if(is.null(FUN))
-  {
-    FUN <- switch(type,
-      xls =,
-      xlsx = readxl::read_excel,
-      csv = ifelse(use.readr, readr::read_delim, read.csv),
-      rds = readRDS,
-      rdata = load,
-      NULL
-    )
-    if(is.null(FUN)) stop("UNHANDLED FILE EXTENSION")
-  }
   FUN(infile, ...)
 }
