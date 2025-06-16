18ad265c002c2ada9cae1a8bdb0859bf54eea6d6
Shawn Garbett <shawn@garbett.org>
2025-06-16 17:28:42 -0500

    Fixed broken tests from renaming

--- tests/testthat/test-020-read_shared-args.R
+++ tests/testthat/test-020-read_shared-args.R
@@ -1,32 +1,32 @@
-context("read_shared")
+context("read_azure")
 
 test_that("drive requires an ms_drive",{
   local_reproducible_output(width = 200)
   expect_error(
-    read_shared(TRUE, "/path/somewhere.csv"),
+    read_azure(TRUE, "/path/somewhere.csv"),
     "Variable 'drive': Must inherit from class 'ms_drive'")
   expect_error(
-    read_shared(NULL, "/path/somewhere.csv"),
+    read_azure(NULL, "/path/somewhere.csv"),
     "Variable 'drive': Must inherit from class 'ms_drive'")
 })
 
 test_that("path is a string",{
   local_reproducible_output(width = 200)
   expect_error(
-    read_shared(structure(NA, class="ms_drive"), TRUE),
+    read_azure(structure(NA, class="ms_drive"), TRUE),
     "Variable 'path': Must be of type 'string'")
   expect_error(
-    read_shared(structure(NA, class="ms_drive"), NULL),
+    read_azure(structure(NA, class="ms_drive"), NULL),
     "Variable 'path': Must be of type 'string'")
   expect_error(
-    read_shared(structure(NA, class="ms_drive"), c('a','b')),
+    read_azure(structure(NA, class="ms_drive"), c('a','b')),
     "Variable 'path': Must have length 1")
 })
 
 test_that("FUN is a function",{
   local_reproducible_output(width = 200)
   expect_error(
-    read_shared(structure(NA, class="ms_drive"), "/a/b.csv", FUN=TRUE),
+    read_azure(structure(NA, class="ms_drive"), "/a/b.csv", FUN=TRUE),
     "Variable 'FUN': Must be a function")
 })
 
