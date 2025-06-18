ec3b2b37fc7824951c54d5793628655738a2bf6c
Shawn Garbett <shawn@garbett.org>
2025-06-18 09:57:15 -0500

    Draft write function

---
+++ R/write_azure.R
@@ -0,0 +1,96 @@
+# Copyright (C) 2024-2025 Shawn Garbett, Cole Beck, James Grindstaff, Lauren
+# Samuels, Vanderbilt University
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+#' Write data from R memory to OneDrive
+#' 
+#' This function given a path will write a file to OneDrive from memory.
+#' 
+#' If \code{FUN} is \code{NULL} it will guess based on file extension. It 
+#' supports: arff, csv, dbf, dta, m, mtp, rds, rdata, rec, spss, syd, sys,
+#' table, xls, xlsx, xpt, yml, and yaml. 
+#' 
+#' @param drive A pointer to an ms_drive object from `Microsoft365R`
+#' @param x The data to be written.
+#' @param path The OneDrive path to write too with filename. 
+#' @param FUN The function to read the data into memory. The default will make a
+#' guess of supported type from the filename extension. The first argument to
+#' the function should be what would normally be the file. This function
+#' defaults to NULL and when that is the case the routine will make a guess
+#' based on the file extension, e.g. if the extension is ".csv" it will use
+#' R's \code{\link{write.csv}}. 
+#' @param \dots Additional arguments to pass to the function that writes the file.
+#' @return An R object containing the requested data.
+#' 
+#' @importFrom Microsoft365R ms_drive_item
+#' @importFrom utils write.csv
+#' @importFrom foreign write.arff write.dbf write.dta 
+#' @importFrom stats write.ftable
+#' @importFrom yaml write_yaml
+#' @importFrom checkmate makeAssertCollection assert_class assert_string assert_function reportAssertions
+#' @export
+#' @seealso \code{\link{link_path}}, \code{\link{owned_azure}}, and \code{\link{shared_azure}}
+#' @examples
+#' \dontrun{
+#' library(Microsoft365R)
+#' drive <- get_business_onedrive(auth_type="device_code")
+#' data  <- write_azure(drive, data, "/SomeDir/sharedata.csv")
+#' }
+write_azure <- function(drive, x, path, FUN=NULL, ...)
+{
+  coll <- checkmate::makeAssertCollection()
+  checkmate::assert_class(x = drive, add = coll, classes="ms_drive")
+  checkmate::assert_string(x = path, add = coll)
+  checkmate::assert_function(x = FUN, add = coll, null.ok = TRUE)
+  checkmate::reportAssertions(coll)
+  
+  filename <- basename(path)
+
+  # TODO: path <- everything but filename
+  
+  ext <- tolower(tools::file_ext(filename))
+  
+  # TODO: Check that extension exists if FUN is NULL
+  
+  item <- get_item_azure(drive, path) 
+  
+  # TODO: Check that item is a folder
+  
+  # If FUN is NULL, guess based on extension
+  if(is.null(FUN))
+  {
+    FUN <- switch(
+      ext,
+      arff  = foreign::write.arff,
+      csv   = utils::write.csv,
+      dbf   = foreign::write.dbf,
+      dta   = foreign::write.dta,
+      rds   = saveRDS,
+      rdata = function(x, file, ...) save(x, file=file, ...), # FIXME Need to preserve variable name
+      rec   = foreign::write.epiinfo,
+      table = stats::write.ftable,
+      yml   = ,
+      yaml  = yaml::write_yaml,
+      NULL
+    )
+    if(is.null(FUN)) stop(paste0("Unhandled File Extension '", ext, "'"))
+  }
+  
+  conn <- rawConnection(raw(0), "w+")
+  on.exit(close(conn))
+  FUN(x, conn, ...)
+  item$upload(src = conn, dest=filename)
+}
